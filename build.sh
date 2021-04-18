763808b294581207dbd637547411ad0f1e8b919a
Ubuntu <ubuntu@ip-172-31-44-86.us-east-2.compute.internal>
2021-04-18 17:56:33 +0000

    asdsadassa

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 sudo docker pull mongo
 sudo docker build -t goapp .
 sudo docker run -d --name mongoinst mongo
-sudo docker run -d --name goappinst goapp
+sudo docker run -d -it --name goappinst goapp
